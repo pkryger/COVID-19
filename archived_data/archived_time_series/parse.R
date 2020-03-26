d7ecaedf22169345d9dd807f9e721af848cb4be9
Przemek Kryger <pkryger@bloomberg.net>
2020-03-26 13:52:27 +0000

    Add inital version for plotting death rate

---
+++ archived_data/archived_time_series/parse.R
@@ -0,0 +1,40 @@
+library(tidyr)
+library(dplyr)
+library(data.table)
+
+deaths_raw <- read.csv("/Users/pkryger/gh/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv")
+
+deaths <- deaths_raw %>%
+    pivot_longer(names_to="date", values_to="cumDeaths", cols=starts_with("X")) %>%
+    filter(cumDeaths != 0) %>%
+    select(-(Lat:Long)) %>%
+    mutate(date=as.Date(date, format="X%m.%d.%y")) %>%
+    rename(country=Country.Region, province=Province.State)
+
+first_deaths <- deaths %>%
+    group_by(country, province) %>%
+    summarize(firstDeath=min(date))
+
+pk.revcumsum <- function(x) {
+    return(x -
+           shift(x, n=1, type="lag", fill=0))
+}
+
+deaths <- left_join(deaths, first_deaths) %>%
+    mutate(offset=date-firstDeath) %>%
+    mutate(dailyDeaths=ave(deaths$cumDeaths,
+                           deaths$country, deaths$province,
+                           FUN = pk.revcumsum))
+
+df <- deaths %>%
+    filter((country == "United Kingdom" & province == "United Kingdom")
+           | country == c("Germany", "Italy", "Spain")
+           | (country == "China" & province == "Hubei"))
+
+ggplot(df, aes(x=offset, y=cumDeaths, color=country)) +
+    geom_point() +
+    geom_line() +
+    scale_y_log10()
+
+ggplot(df, aes(x=offset, y=dailyDeaths, color=country)) +
+    geom_density(stat="identity")
