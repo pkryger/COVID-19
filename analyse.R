e3889f7c27646b7cea862b3466527ee1d1a42b5d
Przemek Kryger <pkryger@bloomberg.net>
2020-03-30 14:44:04 +0100

    More robust interpolation

--- analyse.R
+++ analyse.R
@@ -2,6 +2,7 @@ library(tidyr)
 library(dplyr)
 library(data.table)
 library(ggplot2)
+library(imputeTS)
 
 deaths_raw <- read.csv("/Users/pkryger/gh/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
 
@@ -21,35 +22,20 @@ pk.revcumsum <- function(x) {
            shift(x, n=1, type="lag", fill=0))
 }
 
-pk.smooth <- function(x) {
-    y <- x
-    idx <- which(x %in% 0)
-    # while there are elements that are 0
-    while (0 < length(idx)) {
-        # from first element that is 0
-        first <- idx[1]
-        idx <- idx[-1]
-
-        # find last adjacent element that is 0
-        last <- first
-        while (0 < length(idx) & idx[1] == first + 1) {
-            last <- idx[1]
-            idx <- idx[-1]
+pk.inter <- function(x) {
+    if (length(x) > 2) {
+        y <- table(x)
+        if (!is.na(y["0"]) & length(x) - y["0"] > 2) {
+            return(x %>%
+                   na_if(0) %>%
+                   na_interpolation(option="linear") %>%
+                   replace_na(0))
         }
-        # and go one past that
-        last <- last + 1
-
-        # only in case this is not a series of 0's in the end
-        if (last <= length(x)) {
-            # replace 0's with an average value
-            value <- (x[last] - x[first]) / (last - first + 1)
-            for (i in first:last) {
-                y[i] <- value
-            }
-        }
+        else return(x)
     }
-    return(y)
+    else return(x)
 }
+
 
 deaths <- left_join(deaths, first_deaths) %>%
     mutate(offset=date-firstDeath)
@@ -59,10 +45,10 @@ deaths <- deaths %>%
                            deaths$country, deaths$province,
                            FUN = pk.revcumsum))
 
-    ## %>%
-    ## mutate(dailyDeathsSmooth=ave(deaths$dailyDeaths,
-    ##                              deaths$country, deaths$province,
-    ##                              FUN = pk.smooth))
+deaths <- deaths %>%
+    mutate(dailyDeathsInter=ave(deaths$dailyDeaths,
+                                 deaths$country, deaths$province,
+                                 FUN = pk.inter))
 
 df <- deaths %>%
     filter((country == "United Kingdom" & province == "")
@@ -77,9 +63,9 @@ cumDeaths <- ggplot(df, aes(x=offset, y=cumDeaths, color=country)) +
 dailyDeaths <- ggplot(df, aes(x=offset, y=dailyDeaths, color=country)) +
     geom_density(stat="identity")
 
-dailyDeathsSmooth <- ggplot(df, aes(x=offset, y=dailyDeathsSmooth, color=country)) +
+dailyDeathsInter <- ggplot(df, aes(x=offset, y=dailyDeathsInter, color=country)) +
     geom_density(stat="identity")
 
 ggsave("cumDeaths.png", plot=cumDeaths, dpi=720, width=7, height=7)
 ggsave("dailyDeaths.png", plot=dailyDeaths, dpi=720, width=7, height=7)
-ggsave("dailyDeathsSmooth.png", plot=dailyDeathsSmooth, dpi=720, width=7, height=7)
+ggsave("dailyDeathsInter.png", plot=dailyDeathsInter, dpi=720, width=7, height=7)
