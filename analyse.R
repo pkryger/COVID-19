058e57731b9a8817891c297ae39af522eb7e6b93
Przemek Kryger <pkryger@bloomberg.net>
2020-04-03 17:24:29 +0100

    More robust revcumsum

--- analyse.R
+++ analyse.R
@@ -19,7 +19,8 @@ first_deaths <- deaths %>%
 
 pk.revcumsum <- function(x) {
     return(x -
-           shift(x, n=1, type="lag", fill=0))
+           shift(x, n=1, type="lag") %>%
+           replace_na(x[1]))
 }
 
 pk.inter <- function(x) {
