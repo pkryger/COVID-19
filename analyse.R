2e0309fc99a29a3aa72bd5beba22575e16890b62
Przemek Kryger <pkryger@bloomberg.net>
2020-05-17 13:31:41 +0100

    language server formatting fixups

--- analyse.R
+++ analyse.R
@@ -1,5 +1,4 @@
-library(tidyr)
-library(dplyr)
+library(tidyverse)
 library(data.table)
 library(ggplot2)
 library(imputeTS)
@@ -9,49 +8,61 @@ lookup_raw <- read.csv("csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv")
 lookup <- lookup_raw %>%
     select(-(Lat:Long_)) %>%
     filter(Admin2 == "") %>%
-    rename(country=Country_Region, province=Province_State, population=Population) %>%
-    mutate(population10M=population/10000000)
+    rename(
+        country = Country_Region,
+        province = Province_State,
+        population = Population
+    ) %>%
+    mutate(population10M = population / 10000000)
 
 deaths_raw <- read.csv("csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
 
 deaths <- deaths_raw %>%
-    pivot_longer(names_to="date", values_to="cumDeaths", cols=starts_with("X")) %>%
+    pivot_longer(
+        names_to = "date",
+        values_to = "cumDeaths",
+        cols = starts_with("X")
+    ) %>%
     select(-(Lat:Long)) %>%
-    mutate(date=as.Date(date, format="X%m.%d.%y")) %>%
-    rename(country=Country.Region, province=Province.State) %>%
+    mutate(date = as.Date(date, format = "X%m.%d.%y")) %>%
+    rename(country = Country.Region, province = Province.State) %>%
     group_by(country, province) %>%
     arrange(date)
 
 first_deaths <- deaths %>%
     filter(cumDeaths >= 50) %>%
-    summarize(firstDeath=min(date))
+    summarize(firstDeath = min(date))
 
-pk.revcumsum <- function(x) {
-    x <- x - lag(x, default=x[1])
-    x[x < 0] = 0
+pk_revcumsum <- function(x) {
+    x <- x - lag(x, default = x[1])
+    x[x < 0] <- 0
     return(x)
 }
 
-pk.ratio <- function(x) {
-    return(x / lag(x, default=x[1]))
+pk_ratio <- function(x) {
+    return(x / lag(x, default = x[1]))
 }
 
-pk.inter <- function(x) {
+pk_inter <- function(x) {
     if (length(x) > 2) {
         if (length(x) - length(which(x == 0)) > 2) {
             return(x %>%
-                   na_if(0) %>%
-                   na_interpolation(option="linear") %>%
-                   replace_na(0))
+                na_if(0) %>%
+                na_interpolation(option = "linear") %>%
+                replace_na(0))
         }
-        else return(x)
+        else {
+            return(x)
+        }
     }
-    else return(x)
+    else {
+        return(x)
+    }
 }
 
 ## Input missing values with the next known value divided by the number of
 ## adjacent missing values plus 1. The known value is replaced as well.
-pk.inter2 <- function(x) {
+pk_inter2 <- function(x) {
     if (length(x) < 3 |
         length(x) - length(which(x == 0)) < 3) {
         return(x)
@@ -85,136 +96,194 @@ pk.inter2 <- function(x) {
     return(y)
 }
 
-df <- left_join(deaths %>% filter(cumDeaths >= 50),
-              first_deaths) %>% mutate(day=as.integer(date-firstDeath))
+df <- left_join(
+    deaths %>% filter(cumDeaths >= 50),
+    first_deaths
+) %>%
+    mutate(day = as.integer(date - firstDeath))
 
 df <- df %>%
-    mutate(dailyDeaths=pk.revcumsum(cumDeaths),
-           dailyDeaths=pk.inter2(dailyDeaths),
-           cumDeathsRatio=pk.ratio(cumDeaths),
-           dailyDeathsRatio=pk.ratio(dailyDeaths))
+    mutate(
+        dailyDeaths = pk_revcumsum(cumDeaths),
+        dailyDeaths = pk_inter2(dailyDeaths),
+        cumDeathsRatio = pk_ratio(cumDeaths),
+        dailyDeathsRatio = pk_ratio(dailyDeaths)
+    )
 
 df <- left_join(df, lookup) %>%
-    mutate(cumDeathsNorm=cumDeaths/population10M,
-           dailyDeathsNorm=dailyDeaths/population10M,
-           dailyDeathsNorm=dailyDeaths/population10M)
+    mutate(
+        cumDeathsNorm = cumDeaths / population10M,
+        dailyDeathsNorm = dailyDeaths / population10M,
+        dailyDeathsNorm = dailyDeaths / population10M
+    )
 
 df <- df %>%
     filter((country == "United Kingdom" & province == "")
            | (country == "France" & province == "")
            | country %in% c("Sweden", "Italy", "Spain", "US"))
-#           | (country == "China" & province == "Hubei"))
+#   | (country == "China" & province == "Hubei"))
 
-cumDeaths <- ggplot(df, aes(x=day, y=cumDeaths, color=country)) +
+cumDeaths <- ggplot(df, aes(x = day, y = cumDeaths, color = country)) +
     geom_point() +
     geom_line()
 
-cumDeathsNorm <- ggplot(df, aes(x=day, y=cumDeathsNorm, color=country)) +
+cumDeathsNorm <- ggplot(df, aes(x = day, y = cumDeathsNorm, color = country)) +
     geom_point() +
     geom_line()
 
-dailyDeaths <- ggplot(df, aes(x=day, y=dailyDeaths, color=country)) +
+dailyDeaths <- ggplot(df, aes(x = day, y = dailyDeaths, color = country)) +
     geom_point() +
     geom_line()
 
-dailyDeathsNorm <- ggplot(df, aes(x=day, y=dailyDeathsNorm, color=country)) +
+dailyDeathsNorm <- ggplot(df, aes(x = day, y = dailyDeathsNorm, color = country)) +
     geom_point() +
     geom_line()
 
 
-ggsave("cumDeaths.png", plot=cumDeaths, dpi=720, width=7, height=7)
-ggsave("cumDeathsNorm.png", plot=cumDeathsNorm, dpi=720, width=7, height=7)
-ggsave("dailyDeaths.png", plot=dailyDeaths, dpi=720, width=7, height=7)
-ggsave("dailyDeathsNorm.png", plot=dailyDeathsNorm, dpi=720, width=7, height=7)
+ggsave("cumDeaths.png",
+       plot = cumDeaths, dpi = 720, width = 7, height = 7
+)
+ggsave("cumDeathsNorm.png",
+        plot = cumDeathsNorm, dpi = 720, width = 7, height = 7
+)
+ggsave("dailyDeaths.png",
+       plot = dailyDeaths, dpi = 720, width = 7, height = 7
+)
+ggsave("dailyDeathsNorm.png",
+       plot = dailyDeathsNorm, dpi = 720, width = 7, height = 7
+)
 
-ggsave("dailyDeathsFct.png", plot=dailyDeaths + facet_wrap(~country),
-       dpi=720, width=12, height=7)
-ggsave("dailyDeathsNormFct.png", plot=dailyDeathsNorm + facet_wrap(~country),
-       dpi=720, width=12, height=7)
+ggsave("dailyDeathsFct.png",
+    plot = dailyDeaths + facet_wrap(~country),
+    dpi = 720, width = 12, height = 7
+)
+ggsave("dailyDeathsNormFct.png",
+    plot = dailyDeathsNorm + facet_wrap(~country),
+    dpi = 720, width = 12, height = 7
+)
 
 
 ## Log10 scale
 
 cumDeathsLog10 <- cumDeaths + scale_y_log10()
 dailyDeathsLog10 <- dailyDeaths + scale_y_log10()
-ggsave("cumDeathsLog10.png", plot=cumDeathsLog10, dpi=720, width=7, height=7)
-ggsave("dailyDeathsLog10.png", plot=dailyDeathsLog10, dpi=720, width=7, height=7)
+ggsave("cumDeathsLog10.png",
+       plot = cumDeathsLog10, dpi = 720, width = 7, height = 7
+)
+ggsave("dailyDeathsLog10.png",
+       plot = dailyDeathsLog10, dpi = 720, width = 7, height = 7
+)
 
 # Smoothing
 
 dff <- df %>% filter(day > max(day) - 14)
 
-cumDeathsRatio <- ggplot(df,
-                         aes(x=day, y=cumDeathsRatio, color=country)) +
+cumDeathsRatio <- ggplot(
+    df,
+    aes(x = day, y = cumDeathsRatio, color = country)
+) +
     geom_point() +
     geom_line() +
-    geom_smooth(data=dff,
-                size=0.3,
-                colour="black",
-                linetype="dashed",
-                method=lm,
-                fullrange=TRUE) +
+    geom_smooth(
+        data = dff,
+        size = 0.3,
+        colour = "black",
+        linetype = "dashed",
+        method = lm,
+        fullrange = TRUE
+    ) +
     ylim(1, 1.4) +
-    geom_hline(yintercept=1.055, colour="red", linetype="dashed") +
+    geom_hline(yintercept = 1.055, colour = "red", linetype = "dashed") +
     facet_wrap(~country)
-ggsave("cumDeathsRatio.png", plot=cumDeathsRatio, dpi=720, width=12, height=7)
+ggsave("cumDeathsRatio.png",
+       plot = cumDeathsRatio, dpi = 720, width = 12, height = 7
+)
 
-dailyDeathsRatio <- ggplot(df,
-                           aes(x=day, y=dailyDeathsRatio, color=country)) +
+dailyDeathsRatio <- ggplot(
+    df,
+    aes(x = day, y = dailyDeathsRatio, color = country)
+) +
     geom_point() +
     geom_line() +
-    geom_smooth(data=dff,
-                size=0.3,
-                colour="black",
-                linetype="dashed",
-                method=lm,
-                fullrange=TRUE) +
+    geom_smooth(
+        data = dff,
+        size = 0.3,
+        colour = "black",
+        linetype = "dashed",
+        method = lm,
+        fullrange = TRUE
+    ) +
     ylim(0, 2) +
     facet_wrap(~country)
-ggsave("dailyDeathsRatio.png", plot=dailyDeathsRatio, dpi=720, width=12, height=7)
+ggsave("dailyDeathsRatio.png",
+       plot = dailyDeathsRatio, dpi = 720, width = 12, height = 7
+)
 
 # Modelling
 # see also: https://aosmith.rbind.io/2018/11/16/plot-fitted-lines/
 models <- df %>%
     filter(day > max(day) - 14) %>%
-    do(model = lm(cumDeathsRatio ~ day, .),
-       day = seq(min(.$day), max(df$day) + 8))
+    do(
+        model = lm(cumDeathsRatio ~ day, .),
+        day = seq(min(.$day), max(df$day) + 8)
+    )
 
-predictions <- models %>% do(data.frame(country = .$country,
-                                        province = .$province,
-                                        day = .$day,
-                                        pred = predict(.$model, newdata=.)))
+predictions <- models %>% do(data.frame(
+    country = .$country,
+    province = .$province,
+    day = .$day,
+    pred = predict(.$model, newdata = .)
+))
 predictions <- full_join(df, predictions)
 
-cumDeathsRatioModel <- ggplot(predictions,
-                              aes(x=day, y=cumDeathsRatio, color=country)) +
+cumDeathsRatioModel <- ggplot(
+    predictions,
+    aes(x = day, y = cumDeathsRatio, color = country)
+) +
     geom_point() +
     geom_line() +
-    geom_line(mapping=aes(x=day, y=pred),
-              size=0.3,
-              linetype="dashed") +
+    geom_line(
+        mapping = aes(x = day, y = pred),
+        size = 0.3,
+        linetype = "dashed"
+    ) +
     ylim(1, 1.4) +
-    geom_hline(yintercept=1.055, colour="black", size=0.3, linetype="dashed") +
+    geom_hline(
+        yintercept = 1.055,
+        colour = "black",
+        size = 0.3,
+        linetype = "dashed"
+    ) +
     facet_wrap(~country)
-ggsave("cumDeathsRatioModel.png", plot=cumDeathsRatioModel, dpi=720, width=12, height=7)
+ggsave("cumDeathsRatioModel.png",
+       plot = cumDeathsRatioModel, dpi = 720, width = 12, height = 7
+)
 
 
 # Death ratio
 # Province/State,Country/Region,Lat,Long
 confirmed_raw <- read.csv("csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
 confirmed <- confirmed_raw %>%
-    pivot_longer(names_to="date", values_to="cumConfirmed", cols=starts_with("X")) %>%
+    pivot_longer(
+        names_to = "date",
+        values_to = "cumConfirmed",
+        cols = starts_with("X")
+    ) %>%
     select(-(Lat:Long)) %>%
-    mutate(date=as.Date(date, format="X%m.%d.%y")) %>%
-    rename(country=Country.Region, province=Province.State) %>%
+    mutate(date = as.Date(date, format = "X%m.%d.%y")) %>%
+    rename(country = Country.Region, province = Province.State) %>%
     group_by(country, province) %>%
     arrange(date)
 
-df <- full_join(deaths %>% group_by(date) %>% summarize(deaths=sum(cumDeaths)),
-                confirmed %>% group_by(date) %>% summarize(confirmed=sum(cumConfirmed)))
+df <- full_join(
+    deaths %>% group_by(date) %>% summarize(deaths = sum(cumDeaths)),
+    confirmed %>% group_by(date) %>% summarize(confirmed = sum(cumConfirmed))
+)
 
-deathsRatio <- ggplot(df, aes(x=date, y=deaths/confirmed)) +
+deathsRatio <- ggplot(df, aes(x = date, y = deaths / confirmed)) +
     geom_point() +
     geom_line()
 
-ggsave("deathsRatio.png", plot=deathsRatio, dpi=720, width=7, height=7)
+ggsave("deathsRatio.png",
+       plot = deathsRatio, dpi = 720, width = 7, height = 7
+)
